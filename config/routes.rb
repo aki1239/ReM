46e07bd44ed5555ffd5f9314a89fe78d30c5352c
aki1239 <82556115+aki1239@users.noreply.github.com>
2021-05-28 11:31:26 +0900

    movie 投稿一覧機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
  root to: 'movies#index'
- resources :movies
+ resources :movies, only: [:index, :new, :create]
 end
--- db/migrate/20210525065109_create_movies.rb
+++ db/migrate/20210525065109_create_movies.rb
@@ -5,7 +5,7 @@ class CreateMovies < ActiveRecord::Migration[6.0]
       t.text      :text,      null: false
       t.integer   :genre_id,  null: false
       t.string    :phrase,    null: false
-      t.integer   :user_id,   foreign_key: true, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
