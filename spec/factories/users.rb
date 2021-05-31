bda0656b0158e8a04d696729418d96da2c79256e
aki1239 <82556115+aki1239@users.noreply.github.com>
2021-05-31 18:01:44 +0900

    ユーザー単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { 'a1' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+   
+  end
+end
