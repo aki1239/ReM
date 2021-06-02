9989d0c49b43881e90ffefb136068d092151fee5
aki1239 <82556115+aki1239@users.noreply.github.com>
2021-06-02 10:57:19 +0900

    setTimeout

---
+++ app/javascript/movie.js
@@ -0,0 +1,17 @@
+
+/*window.addEventListener('load', function(){
+
+document.getElementById("test").addEventListener("mouseover",function(){
+  setTimeout(function(){
+
+    result = prompt('了解　と入力してください')
+   if ( result === '了解') {
+        alert('了解と入力されました');
+    }
+    else {
+        alert('了解と入力されませんでした');
+    }
+  },4000);
+});
+})*/
+
